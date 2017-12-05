3183a25a181695cb4b37c7b8c55c9c1939b52d3c
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-12-05 10:41:49 -0800

    add server side files and install dependencies

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const eventController = require('./controllers/eventController');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(logger('short'));
+
+app.use((req, res) => {
+  res.status(404);
+  res.send('File not found!');
+});
+
+app.post('/createUser', eventController.createUser);
+
+app.get('/getCharityInfo', eventController.getCharityInfo);
+
+
+app.listen(3000, () => {
+  console.log('App started on port 3000');
+});
