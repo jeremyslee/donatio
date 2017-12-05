a3cc88682eec0c03bd73203bb2f36ae131b57b40
Katherine Sampias <ksampias@gmail.com>
2017-12-05 12:50:42 -0800

    added webpack config

--- server/server.js
+++ server/server.js
@@ -4,16 +4,22 @@ const eventController = require('./controllers/eventController');
 const logger = require('morgan');
 const scrapeController = require('./scraper');
 
+const path = require('path')
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+
 const app = express();
+
+const PUBLIC_DIR = path.join(__dirname, "../public");
+app.use(express.static(PUBLIC_DIR));
 
 app.use(bodyParser.json());
 
-// app.use(logger('short'));
+app.use(logger('short'));
 
-// app.use((req, res) => {
-//   res.status(404);
-//   res.send('File not found!');
-// });
+app.get("/", function(req, res) {
+  res.sendFile(path.join(PUBLIC_DIR, 'index.html'))
+})
 
 app.post('/createUser', eventController.createUser);
 
@@ -21,6 +27,10 @@ app.get('/getCharityInfo', eventController.getCharityInfo);
 
 app.get('/crawler', scrapeController.scrapeCharity)
 
+app.use((req, res) => {
+  res.status(404);
+  res.send('File not found!');
+});
 
 app.listen(3000, () => {
   console.log('App started on port 3000');
