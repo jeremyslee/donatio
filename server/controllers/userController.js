d04a41524c2113ece5b07bc4cfe2495c74776bcb
Jelena Gruica <jelena.gruica@gmail.com>
2017-12-06 14:54:53 -0800

    bcrypt finished

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,17 @@
 const db = require('../models/database');
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 const userController = {
     postLogin(req, res, next) {
-        const query = `SELECT * FROM "user" WHERE username = '${req.body.usernameLogin}' AND password = '${req.body.passwordLogin}'`;
+        const query = `SELECT * FROM "user" WHERE username = '${req.body.usernameLogin}'`;
         db.conn.one(query)
             .then(postLogin => {
-                res.status(200).send({ 'msg': 'Login successful' });
+                if (bcrypt.compareSync(req.body.passwordLogin, postLogin.password)) {
+                    res.status(200).send({ 'msg': 'Login successful' });
+                }else {
+                    console.log('Password dont match')
+                }
             })
             .catch(err => {
                 console.log('The error is', err);
@@ -22,19 +27,21 @@ const userController = {
             last_name: req.body.lastname,
         });
 
-        let query = {
-            text: 'INSERT INTO "user" (email, username, password, first_name, last_name) VALUES($1, $2, $3, $4, $5)',
-            values: [newUser.email, newUser.username, newUser.password, newUser.first_name, newUser.last_name]
-        };
-        db.conn.none(query)
-            .then(createUser => {
-                res.status(200).send({ 'msg': 'User created!'});
-            })
-            .catch(err => {
-                console.log('The error is', err);
-                res.status(404).send(err)
-            });
-    }
 
+        bcrypt.hash(req.body.password, 10, function (err, hash) {
+            let query = {
+                text: 'INSERT INTO "user" (email, username, password, first_name, last_name) VALUES($1, $2, $3, $4, $5)',
+                values: [newUser.email, newUser.username, hash, newUser.first_name, newUser.last_name]
+            }
+            db.conn.none(query)
+                .then(createUser => {
+                    res.status(200).send({ 'msg': 'User created!' });
+                })
+                .catch(err => {
+                    console.log('The error is', err);
+                    res.status(404).send(err)
+                });
+        })
+    }
 }
 module.exports = userController;
